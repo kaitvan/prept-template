1b167b43cbf47a1537eec464972ab0021b56eded
Kaitlyn VanHook <kaitlynvanhook@Kaitlyns-MacBook-Pro-2.local>
2020-11-30 20:51:58 -0600

    caught up with class

--- src/helpers/data/questionData.js
+++ src/helpers/data/questionData.js
@@ -1,7 +1,11 @@
-// const baseUrl = '';
+import axios from 'axios';
+
+const baseUrl = 'https://prept-b690a.firebaseio.com/-MNR7ko6it8emIzeTaDq';
 
 const getQuestions = () => new Promise((resolve, reject) => {
-
+  axios.get(`${baseUrl}.json`).then((response) => {
+    resolve(Object.values(response.data)).catch((error) => reject(error));
+  });
 });
 
 export default { getQuestions };
