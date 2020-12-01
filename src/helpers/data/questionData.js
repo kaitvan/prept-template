2ea9251b946fb89455d81ce1f5d307b97c96e871
Kaitlyn VanHook <kaitlynvanhook@Kaitlyns-MacBook-Pro-2.local>
2020-11-30 20:55:34 -0600

    updated get request

--- src/helpers/data/questionData.js
+++ src/helpers/data/questionData.js
@@ -4,8 +4,8 @@ const baseUrl = 'https://prept-b690a.firebaseio.com/-MNR7ko6it8emIzeTaDq';
 
 const getQuestions = () => new Promise((resolve, reject) => {
   axios.get(`${baseUrl}.json`).then((response) => {
-    resolve(Object.values(response.data)).catch((error) => reject(error));
-  });
+    resolve(Object.values(response.data));
+  }).catch((error) => reject(error));
 });
 
 export default { getQuestions };
